1f744609dfb5a4a7b47f6254f26d80f1f016d310
WilliamFloydJ <floydfamily71@gmail.com>
2022-05-07 07:55:04 -0600

    Button Addition

--- Server/index.js
+++ Server/index.js
@@ -29,7 +29,7 @@ app.get("/css", (req, res) => {
   res.sendFile(path.join(__dirname, "../Public/main.css"));
 });
 
-app.use("/js", express.static(path.join(__dirname, "../Public/main.js")));
+app.use("/js", express.static(path.join(__dirname, "../Public/index.js")));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../Public/index.html"));
