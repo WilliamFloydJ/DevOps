cfef4ae3ea3ce746e028b9f7e0825c6a324b02af
WilliamFloydJ <floydfamily71@gmail.com>
2022-05-06 22:03:32 -0600

    Initial Commit

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,28 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const { create } = require("domain");
+var rollbar = new Rollbar({
+  accessToken: "a65031504a4d4c429fd52e782dabf5e9",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+app.get("/create", (req, res) => {
+  try {
+    create();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../Public/index.html"));
 });
